9845f107e7403d298e83a02e2f9dc0407cff73cf
vlasisPi <vlassisp89@gmail.com>
2021-08-28 12:47:20 +0300

    Controllers, views and dynamic content

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Root page. Triggered by http://localhost:3000/
+  root 'demo#index'
+
+  # OR you can write it as =>
+  # match "demo/index", :to => "demo#index", :via => :get
+  get 'demo/index'
+
+  # default route
+  # triggered by http://localhost:3000/demo/index, http://localhost:3000/demo  (default is index action)
+  # get ':controller(/:action(/:id))'
 end
+
